bdef912effed0edbe2e482da5da7ef8a3649f134
Harjas Singh <harjas42@icloud.com>
2026-06-06 17:10:00 +0530

    Create main Spring Boot application class

---
+++ src/main/java/com/sysgrid/SysGridApplication.java
@@ -0,0 +1,13 @@
+package com.sysgrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysGridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysGridApplication.class, args);
+	}
+
+}
