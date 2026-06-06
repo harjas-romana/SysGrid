d6af4317f3de5e2a8ee4c465296a220a5df25bd0
Harjas Singh <harjas42@icloud.com>
2026-06-06 18:30:00 +0530

    Add initial context load test

---
+++ src/test/java/com/sysgrid/SysGrid/SysGridApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysgrid.SysGrid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysGridApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
