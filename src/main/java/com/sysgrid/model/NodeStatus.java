7b4023b4b5119c2473972968cc642efe4ab80c0a
Harjas Singh <harjas42@icloud.com>
2026-06-07 09:15:00 +0530

    Define node enums for type and status

---
+++ src/main/java/com/sysgrid/model/NodeStatus.java
@@ -0,0 +1,9 @@
+package com.sysgrid.model;
+
+public enum NodeStatus {
+
+    HEALTHY,
+    OVERLOADED,
+    WARNING,
+    OFFLINE
+}
---
+++ src/main/java/com/sysgrid/model/NodeType.java
@@ -0,0 +1,10 @@
+package com.sysgrid.model;
+
+public enum NodeType {
+
+    CLIENT,
+    LOAD_BALANCER,
+    DATABASE,
+    CACHE,
+    APP_SERVER
+}
